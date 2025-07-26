bb85d63ccfea328337bfb061e36dc2c3038a0d70
TomeHane <95963977+TomeHane@users.noreply.github.com>
2025-07-26 19:08:23 +0000

    routes.rbなどを編集

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  root 'static_pages#home' # => root(/)で表示するページをhomeに変更
-  get 'static_pages/home' # => StaticPages#home URLとコントローラー名#アクション名が密結合している書き方（あまり使わない）
-  get 'static_pages/help'
-  get 'static_pages/about'
-  get  "static_pages/contact"
+  root 'static_pages#home'
+  get  '/help',    to: 'static_pages#help'
+  get  '/about',   to: 'static_pages#about'
+  get  '/contact', to: 'static_pages#contact'
 end
