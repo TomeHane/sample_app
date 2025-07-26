f560a5855f24c20e1b618823c4d0bc07ae4328a5
TomeHane <95963977+TomeHane@users.noreply.github.com>
2025-07-26 18:46:49 +0000

    contactページの追加

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   get 'static_pages/home' # => StaticPages#home URLとコントローラー名#アクション名が密結合している書き方（あまり使わない）
   get 'static_pages/help'
   get 'static_pages/about'
+  get  "static_pages/contact"
 end
