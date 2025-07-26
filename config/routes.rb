cc1769c91b751039e1cdd27bc80f3646524e7302
TomeHane <95963977+TomeHane@users.noreply.github.com>
2025-07-26 07:19:37 +0000

    root(/)で表示するページをhomeに変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
+  root 'static_pages#home' # => root(/)で表示するページをhomeに変更
   get 'static_pages/home' # => StaticPages#home
   get 'static_pages/help'
   get 'static_pages/about'
-  root "hello#index"
 end
