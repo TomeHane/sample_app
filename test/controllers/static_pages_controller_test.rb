eb9110b217fcaa628894989c655b05a09126ece6
TomeHane <95963977+TomeHane@users.noreply.github.com>
2025-07-26 07:03:38 +0000

    テストを追加

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,18 +1,21 @@
-require "test_helper"
+require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-  test "should get home" do
+  test 'should get home' do
     get static_pages_home_url
     assert_response :success # レスポンスが正常に返ってくるか？
+    assert_select 'title', 'Home | Ruby on Rails Tutorial Sample App' # <title>タグの中身が一致しているか？
   end
 
-  test "should get help" do
+  test 'should get help' do
     get static_pages_help_url
     assert_response :success
+    assert_select 'title', 'Help | Ruby on Rails Tutorial Sample App'
   end
 
-  test "should get about" do
+  test 'should get about' do
     get static_pages_about_url
     assert_response :success
+    assert_select 'title', 'About | Ruby on Rails Tutorial Sample App'
   end
 end
