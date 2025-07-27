d8420df1193eea023af07e272dfc2904cadb98b2
TomeHane <95963977+TomeHane@users.noreply.github.com>
2025-07-27 07:23:44 +0000

    マイグレーションファイルの生成&実行

--- db/migrate/20250727061937_create_users.rb
+++ db/migrate/20250727061937_create_users.rb
@@ -1,5 +1,6 @@
 class CreateUsers < ActiveRecord::Migration[7.0]
   def change
+    # create_tableメソッドの呼び出し。tには、create_tableメソッドにより生成されたテーブルが渡される
     create_table :users do |t|
       t.string :name
       t.string :email
