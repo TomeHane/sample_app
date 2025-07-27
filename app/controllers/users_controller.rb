cadcbb280865a3607518f6313d448e1b71e921f4
TomeHane <95963977+TomeHane@users.noreply.github.com>
2025-07-27 16:34:41 +0000

    ユーザ詳細画面を作る

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,10 @@
 class UsersController < ApplicationController
-  def new
+  # GET /users/:id
+  def show
+    @user = User.find(params[:id])
+    # このタイミングで処理を止める（Railsサーバのターミナルで確認できる）
+    # debugger
   end
+
+  def new; end
 end
