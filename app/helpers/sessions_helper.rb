0834fb90451847bdbd77a9aeab9e2683357e8835
TomeHane <95963977+TomeHane@users.noreply.github.com>
2025-07-28 08:19:25 +0000

    current_userメソッドについてコメントを追加

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -5,7 +5,10 @@ module SessionsHelper
   end
 
   # 現在ログイン中のユーザーを返す（いる場合）
-  # if文を書くことで、無駄にDBにクエリ（User.find_by）を送らないようにしている
+  # 1.if文を書くことで、無駄にDBにクエリ（User.find_by）を送らないようにしている
+  # 2.最初にクエリ結果をインスタンス変数にUserオブジェクトを格納しておき
+  #   以降はインスタンス変数を返す（メモ化）
+  # => DBへの問い合わせが極力少なくなる
   def current_user
     if session[:user_id]
       @current_user ||= User.find_by(id: session[:user_id])
