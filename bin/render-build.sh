d85b8aade4b9d2d76498665966656adcdabcf558
TomeHane <95963977+TomeHane@users.noreply.github.com>
2025-07-29 11:07:29 +0000

    ビルドスクリプトにdb:migrate:resetとdb:seedを追加する

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -5,3 +5,7 @@ bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
 bundle exec rails db:migrate
+
+# DBデータをクリアし、サンプルデータを追加する（チュートリアル固有）
+DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rails db:migrate:reset
+bundle exec rails db:seed
